036b4fd22b07581f841334eccbcafac9dbfe684d
rylan-dunning <brdunning14@gmail.com>
2025-05-04 15:01:26 -0600

    finished up the ability to post

--- services/index.js
+++ services/index.js
@@ -33,7 +33,100 @@ export const getPosts = async () => {
         }
     `
     const result = await request(graphqlAPI, query);
+    
 
     return result.postsConnection.edges;
 
+};
+
+export const getRecentPosts = async () => {
+    const query = gql`
+        query getPostDetails {
+            posts(
+                orderBy: createdAt_ASC
+                last: 3
+            ) {
+                title
+                featuredImage {
+                    url
+                }
+                createdAt
+                slug
+            }
+        }  
+    `
+    const result = await request(graphqlAPI, query);
+
+    return result.posts;
+}
+
+export const getSimilarPosts = async ( categories, slug ) => {
+    const query = gql`
+        query GetPostDetails($slug: String!, $category: [String!]) {
+            posts(
+                where: { slug_not: $slug, AND: { category_some: { slug_in: $category}}} 
+                last: 3
+            ) {
+                title
+                featuredImage {
+                    url
+                }
+                createdAt
+                slug
+            }
+        }
+    `;
+    const result = await request(graphqlAPI, query, { categories, slug });
+
+    return result.posts;
+}
+// 
+export const getPostDetails = async (slug) => {
+    const query = gql`
+        query GetPostDetails($slug : String!) {
+            post(where: {slug: $slug }) {
+                author {
+                    bio
+                    name
+                    id
+                    photo {
+                        url
+                    }
+                }
+                createdAt
+                slug
+                title
+                excerpt
+                featuredImage {
+                    url
+                }
+                category {
+                    name
+                    slug
+                }
+                content {
+                    raw
+                }
+            }
+        }
+    `;
+
+    const result = await request(graphqlAPI, query, { slug });
+    
+    return result.post;
+
+};
+
+export const getCategories = async () => {
+    const query = gql`
+        query GetCategories {
+            categories {
+                name
+                slug
+            }
+        }
+    `
+    const result = await request(graphqlAPI, query);
+
+    return result.categories;
 }
